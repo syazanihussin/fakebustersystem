5a4d45a6c997b55da988170c0d682e462d8703d6
syazanihussin <syazanihussin@gmail.com>
2019-11-16 22:33:03 +0800

    update

--- system/extractor.py
+++ system/extractor.py
@@ -22,6 +22,7 @@ class Extractor(implements(IExtractor)):
 
         if(self.check_url(url) == 200):
             try:
+                print('url', url);
                 extracted_news = requests.get('http://202.45.142.95/readibility/text.php?base64url=' + url).text
                 if extracted_news == "Looks like we couldn't find the content.":
                     return 'error extract1'
